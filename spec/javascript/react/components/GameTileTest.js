64e93f848bcb08201bec02da9469cf66b73f73d3
Jack <jack21222@gmail.com>
2018-02-07 19:20:04 -0500

    added test for game tiles

---
+++ spec/javascript/react/components/GameTileTest.js
@@ -0,0 +1,31 @@
+import GameTile from '../../../../app/javascript/react/components/GameTile.js'
+
+describe('GameTile', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(
+      <GameTile
+        name="Game One"
+        description="This is a description"
+      />
+    )
+  })
+
+
+  it('should have an h2 tag', () => {
+    expect(wrapper.find('h2').length).toEqual(1)
+  })
+
+  it('should have expected content in h2 tag', () => {
+    expect(wrapper.find('h2').text()).toBe('Game One')
+  })
+
+  it('should have a p tag', () => {
+    expect(wrapper.find('p').length).toEqual(1)
+  })
+
+  it('should have expected content in h6 tag', () => {
+    expect(wrapper.find('p').text()).toBe('Description: This is a description')
+  })
+})
