959df76a8e4d4cb551003e68a040070e7cdbd959
Jack <jack21222@gmail.com>
2018-02-08 19:06:17 -0500

    added tests for clocks

---
+++ spec/javascript/react/components/ClockTileTest.js
@@ -0,0 +1,25 @@
+import ClockTile from '../../../../app/javascript/react/components/ClockTile.js'
+
+describe('GameTile', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(
+      <ClockTile
+      key = '1'
+      id = '1'
+      />
+    )
+  })
+
+
+  it('should have an h3 tag', () => {
+    expect(wrapper.find('h3').length).toEqual(1)
+  })
+
+  it('should have expected content in h3 tag', () => {
+    expect(wrapper.find('h3').text()).toBe('Clocks')
+  })
+
+
+})
